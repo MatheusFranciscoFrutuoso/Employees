fa65ba7362e73943f1b13842c6a774940442b598
rafinha-96 <rafa10_zc@hotmail.com>
2021-08-13 13:40:48 -0300

    agorasim  Closed #4

--- Employees/Data/Models/Profiles.cs
+++ Employees/Data/Models/Profiles.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Data.Models
 {
-    class Profiles
+    class Profiles : Base
     {
         public string AcessLevel { get; set; }
         public string Description { get; set; }
