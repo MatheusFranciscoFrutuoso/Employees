352139dd4fd76cb32100a169a754787acf8f10f5
vinicius <vinicius_stumpf@edu.univali.br>
2021-08-13 15:16:20 -0300

    add repository of position Closed #14

---
+++ Employees/Data/Repository/PositionRepository.cs
@@ -0,0 +1,31 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    class PositionRepository:BaseRepository<Position>
+    {
+
+        Position position = new Position();
+
+        Employees employees = new Employees();
+
+
+
+        public void setPosition( int id_Employee, int id_Position)
+        {
+
+            RepositoryEmployees repor = new RepositoryEmployees();
+            position = Read(id_Position);
+
+            employees = repor.Read(id_Employee);
+        }
+
+
+
+    }
+}
