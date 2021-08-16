3a5aac7335444eddfe53d205e6e37826bbf0d6dc
Matheus Frutuoso <matheusfx5@gmail.com>
2021-08-16 09:35:47 -0300

    Create composition Team Closed #45

---
+++ Employees/Data/Migrations/202108161235030_CreateCompositionTeam.cs
@@ -0,0 +1,22 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateCompositionTeam : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Employees", "Team_Id", c => c.Int());
+            CreateIndex("dbo.Employees", "Team_Id");
+            AddForeignKey("dbo.Employees", "Team_Id", "dbo.Teams", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Employees", "Team_Id", "dbo.Teams");
+            DropIndex("dbo.Employees", new[] { "Team_Id" });
+            DropColumn("dbo.Employees", "Team_Id");
+        }
+    }
+}
