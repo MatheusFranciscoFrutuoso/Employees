ee67ebd8305a9660eb9fb61c3291022d9c71f803
Matheus Frutuoso <matheusfx5@gmail.com>
2021-08-16 09:10:42 -0300

    Create migration InviteSatate Closed #38

---
+++ Employees/Data/Migrations/202108161209041_CreateTableInviteState.cs
@@ -0,0 +1,29 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTableInviteState : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.InviteStates",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        State = c.String(),
+                        DateSend = c.DateTime(nullable: false),
+                        DateResponse = c.DateTime(nullable: false),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.InviteStates");
+        }
+    }
+}
