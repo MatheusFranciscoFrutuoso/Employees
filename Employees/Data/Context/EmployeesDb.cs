b5387a4e5ecfeb94257c96e05d1e6d9fbfb7cf87
Matheus Frutuoso <matheusfx5@gmail.com>
2021-08-16 09:26:15 -0300

    Create Migration Employees Closed #34

--- Employees/Data/Context/EmployeesDb.cs
+++ Employees/Data/Context/EmployeesDb.cs
@@ -13,6 +13,7 @@ namespace Data.Context
         public DbSet<InviteState> InviteState { get; set; }
         public DbSet<Profiles> Profiles { get; set; }
         public DbSet<Position> Position { get; set; }
+        public DbSet<Employees> Employees { get; set; }
         public EmployeesDb():base ("Data Source=192.168.0.114;Initial Catalog=Employees;Persist Security Info=True;User ID=Employees;Password=projetox")
         {
 
